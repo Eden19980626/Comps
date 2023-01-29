498718b04f51fc26dc020e612e88cd15ad78d391
Dengz <dengweiyi870626@gmail.com>
2023-01-29 17:31:42 +0800

    Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import Button from './components/Button';
+import Accordion from './components/Accordion';
 function App() {
-  return (
-    <div>
-      <Button primary>123</Button>
-      <Button danger>hi</Button>
-      <Button secondary>123</Button>
-    </div>
-  );
+  const items = [
+    { id: '1', label: 'Can I use React on project?', content: 'Yes1' },
+    { id: '2', label: 'Can I use React on project?', content: 'Yes2' },
+    { id: '3', label: 'Can I use React on project?', content: 'Yes3' },
+  ];
+  return <Accordion items={items} />;
 }
 export default App;
