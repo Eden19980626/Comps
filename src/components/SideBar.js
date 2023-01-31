5cae5ff2c6084fd11c04eb5c0fccb943e5d16622
Dengz <dengweiyi870626@gmail.com>
2023-01-31 18:51:19 +0800

    Highlighting the Active Link

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -9,12 +9,19 @@ function SideBar() {
 
   const rederedLinks = links.map((link) => {
     return (
-      <Link key={link.label} to={link.path}>
+      <Link
+        key={link.label}
+        to={link.path}
+        className='mb-3'
+        activeClassName='font-bold border-l-4 border-blue-500 pl-2'
+      >
         {link.label}
       </Link>
     );
   });
-  return <div className='sticky top-0 flex flex-col '>{rederedLinks}</div>;
+  return (
+    <div className='sticky top-0 flex flex-col items-start'>{rederedLinks}</div>
+  );
 }
 
 export default SideBar;
